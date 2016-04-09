83513dca4b2fd5b506498a3cdad6c3987a76584b
Wolfgang <wolf@okwolf.com>
2016-04-09 14:32:12 -0700

    fixed misplacement of image name in docker run

--- run.sh
+++ run.sh
@@ -4,8 +4,9 @@ if docker info &> /dev/null ; then
     docker build -t deploy deploy
 	docker run --rm -ti -v ~/.ssh:/root/.ssh \
 	-e DO_TOKEN -e DO_SSH_KEY_FINGERPRINT -e DO_DOCKER_BUILD_REPO \
-	-e DO_DOCKER_RUN_OPTIONS deploy \
-	-e DO_NAME_PREFIX -e DO_REGION -e DO_SIZE -e DO_CHANNEL
+	-e DO_DOCKER_RUN_OPTIONS \
+	-e DO_NAME_PREFIX -e DO_REGION -e DO_SIZE -e DO_CHANNEL \
+	deploy
 else
     deploy/deploy.sh
 fi
