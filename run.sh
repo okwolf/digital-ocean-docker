89b5d66984fbc48f3a2843d46e8decdfbbb88162
Wolfgang <wolf@okwolf.com>
2016-04-18 11:31:36 -0700

    fixed typo in docker run command

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ if docker info &> /dev/null ; then
 	# If Docker available, use it for maximum sanity
     docker build -t deploy deploy
 	docker run --rm -ti -v ~/.ssh:/root/.ssh \
-	-e DO_TOKEN -e DO_SSH_KEY_FINGERPRINT
+	-e DO_TOKEN -e DO_SSH_KEY_FINGERPRINT \
 	-e DO_NAME_PREFIX -e DO_REGION -e DO_SIZE -e DO_CHANNEL \
 	-e DO_DOCKER_BUILD_REPO -e DO_DOCKER_RUN_OPTIONS \
 	deploy
